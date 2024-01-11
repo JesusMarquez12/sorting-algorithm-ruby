be4d649d7f9db3d174e8f1b82e7948a7659f4639
Jesús Márquez <jesus.marquez.ingelo@gmail.com>
2024-01-11 12:09:35 -0300

    implemented recursion and merge to merge sort method

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,13 +1,18 @@
 #!/usr/bin/env ruby
 
-my_array_1 = Array.new(3) {rand(1..9)}
-puts "Unsorted array 1: \t#{my_array_1}"
+array_1 = Array.new(5) {rand(1..9)}
+puts "Unsorted array 1: \t#{array_1}"
 
-my_array_2 = Array.new(6) {rand(1..9)}
-puts "Unsorted array 2: \t#{my_array_2}"
+sorted_array_1 = merge_sort(array_1)
 
-merged_array = merge(my_array_1, my_array_2)
-puts "Merge array: \t\t#{merged_array}"
+puts "Sorted array 1: \t#{sorted_array_1}", ""
+
+array_2 = Array.new(6) {rand(1..9)}
+puts "Unsorted array 2: \t#{array_2}"
+
+sorted_array_2 = merge_sort(array_2)
+
+puts "Sorted array 2: \t#{sorted_array_2}"
 
 BEGIN {
   def merge(left_array, right_array)
@@ -30,11 +35,11 @@ BEGIN {
     index_mid = my_array.length / 2
 
     left_array = my_array[...index_mid]
-    puts "left array: \t\t#{left_array}"
-
     right_array = my_array[index_mid...]
-    puts "right array: \t\t#{right_array}"
 
-    return my_array
+    sorted_left_array = merge_sort(left_array)
+    sorted_right_array = merge_sort(right_array)
+
+    return merge(sorted_left_array, sorted_right_array)
   end
 }
