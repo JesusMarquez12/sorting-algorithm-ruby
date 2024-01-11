b41638e922797ce4345a67a258b695b6c0d0b868
Jesús Márquez <jesus.marquez.ingelo@gmail.com>
2024-01-11 12:04:53 -0300

    Implemented merge method

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,20 +1,29 @@
 #!/usr/bin/env ruby
 
-my_array_1 = Array.new(5) {rand(1..9)}
+my_array_1 = Array.new(3) {rand(1..9)}
 puts "Unsorted array 1: \t#{my_array_1}"
-
-merge_sort(my_array_1)
-
-puts "Sorted array 1: \t#{my_array_1}", ""
 
 my_array_2 = Array.new(6) {rand(1..9)}
 puts "Unsorted array 2: \t#{my_array_2}"
 
-merge_sort(my_array_2)
-
-puts "Sorted array 2: \t#{my_array_2}"
+merged_array = merge(my_array_1, my_array_2)
+puts "Merge array: \t\t#{merged_array}"
 
 BEGIN {
+  def merge(left_array, right_array)
+    result_array = []
+
+    while left_array.length > 0 && right_array.length > 0
+      min_value = left_array.first > right_array.first ? right_array.shift : left_array.shift
+      result_array.append(min_value)
+    end
+
+    result_array.append(left_array.shift) while left_array.length > 0
+    result_array.append(right_array.shift) while right_array.length > 0
+
+    return result_array
+  end
+
   def merge_sort(my_array)
     return my_array if my_array.length == 1
 
