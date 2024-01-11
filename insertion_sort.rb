63d3e91681bb521c1e47b55b6a9926e4b2cf001e
Jesús Márquez <jesus.marquez.ingelo@gmail.com>
2024-01-11 10:22:42 -0300

    update insertion sort index name to standarize with future files

--- insertion_sort.rb
+++ insertion_sort.rb
@@ -5,14 +5,14 @@ puts "Unsorted array: \t#{my_array}"
 
 array_length = my_array.length
 
-for j in 1...array_length
-  key = my_array[j]
-  i = j - 1
-  while(i >= 0 && my_array[i] > key)
-    my_array[i + 1] = my_array[i]
-    i -= 1
+for i in 1...array_length
+  key = my_array[i]
+  j = i - 1
+  while(j >= 0 && my_array[j] > key)
+    my_array[j + 1] = my_array[j]
+    j -= 1
   end
-  my_array[i + 1] = key
+  my_array[j + 1] = key
 end
 
 puts "Sorted array: \t\t#{my_array}"
